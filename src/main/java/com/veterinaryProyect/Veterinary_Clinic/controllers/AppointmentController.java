d56e3c60de9ce26875b43b3720180d888dca4a2f
FranciscoJChCano <franciscojcharnecocano@gmail.com>
2024-07-22 15:17:02 +0200

    create read patient/appointment

--- src/main/java/com/veterinaryProyect/Veterinary_Clinic/controllers/AppointmentController.java
+++ src/main/java/com/veterinaryProyect/Veterinary_Clinic/controllers/AppointmentController.java
@@ -1,10 +1,15 @@
 package com.veterinaryProyect.Veterinary_Clinic.controllers;
 
 
+import com.veterinaryProyect.Veterinary_Clinic.models.Appointment;
 import com.veterinaryProyect.Veterinary_Clinic.services.AppointmentServices;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("*")
@@ -12,5 +17,14 @@ public class AppointmentController {
 
     @Autowired
     AppointmentServices appointmentServices;
+
+    public List<Appointment> getAllAppointment() {
+        return appointmentServices.getAllAppointment();
+    }
+
+    @GetMapping(path = "/*/{id}")
+    public Appointment getAppointmentById(@PathVariable("id") Long id) {
+        return appointmentServices.getById(id);
+    }
 
 }
