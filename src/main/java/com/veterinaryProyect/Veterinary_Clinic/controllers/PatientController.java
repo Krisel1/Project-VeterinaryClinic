4bcb9c6f6275ea71b9c5a2537b23bf02a4a01a58
Raunier24 <peplisoft@gmail.com>
2024-07-24 14:05:42 +0200

    fixing error

---
+++ src/main/java/com/veterinaryProyect/Veterinary_Clinic/controllers/PatientController.java
@@ -0,0 +1,33 @@
+package com.veterinaryProyect.Veterinary_Clinic.controllers;
+
+
+import com.veterinaryProyect.Veterinary_Clinic.models.Patient;
+import com.veterinaryProyect.Veterinary_Clinic.services.PatientServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/")
+public class PatientController {
+
+    @Autowired
+    PatientServices patientServices;
+
+    @GetMapping(path = "/patients")
+    public List<Patient> getAllPatient() {
+        return patientServices.getAllPatient();
+    }
+
+    @GetMapping(path = "/patient/{id}")
+    public Patient getPatientById(@PathVariable("id") Long id) {
+        return patientServices.getById(id);
+    }
+
+    @DeleteMapping(path = "/patient/{id}")
+    public void deletePatientById(@PathVariable("id") Long id) {patientServices.deletePatient(id); }
+
+
+}
