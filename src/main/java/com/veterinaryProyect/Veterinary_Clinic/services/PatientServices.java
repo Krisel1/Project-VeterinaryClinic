494184eeee4b8036215a0d23fef7fd11e3c1ba31
Raunier <peplisoft@gmail.com>
2024-07-24 23:58:20 +0200

    Update PatientServices.java

--- src/main/java/com/veterinaryProyect/Veterinary_Clinic/services/PatientServices.java
+++ src/main/java/com/veterinaryProyect/Veterinary_Clinic/services/PatientServices.java
@@ -1,11 +1,10 @@
 package com.veterinaryProyect.Veterinary_Clinic.services;
 
-
 import com.veterinaryProyect.Veterinary_Clinic.models.Patient;
 import com.veterinaryProyect.Veterinary_Clinic.repositories.IPatientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import java.util.List;
 
 @Service
 public class PatientServices {
@@ -13,9 +12,17 @@ public class PatientServices {
     @Autowired
     IPatientRepository iPatientRepository;
 
-
-    public Patient createPatient(Patient patient) {
-        return iPatientRepository.save(patient);
+    public List<Patient> getAllPatient() {
+        return (List<Patient>) iPatientRepository.findAll();
     }
-}
 
+    public Patient getById(Long id) {
+        Patient patient;
+        patient = iPatientRepository.findById(id).orElseThrow();
+        return patient;
+
+    }
+
+    public void deletePatient(long id) {iPatientRepository.deleteById(id); }
+
+}
