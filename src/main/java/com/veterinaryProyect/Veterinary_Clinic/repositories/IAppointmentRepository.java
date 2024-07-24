c4b9da2823d90c3fc60235f8f265e7c76c3b86c3
Raunier24 <peplisoft@gmail.com>
2024-07-24 13:33:05 +0200

    Error2

--- src/main/java/com/veterinaryProyect/Veterinary_Clinic/repositories/IAppointmentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.veterinaryProyect.Veterinary_Clinic.repositories;
-
-import com.veterinaryProyect.Veterinary_Clinic.models.Appointment;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface IAppointmentRepository extends CrudRepository<Appointment, Long> {
-}
--- src/main/java/com/veterinaryProyect/Veterinary_Clinic/repositories/IPatientRepository.java
+++
@@ -1,8 +0,0 @@
-package com.veterinaryProyect.Veterinary_Clinic.repositories;
-
-import com.veterinaryProyect.Veterinary_Clinic.models.Patient;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface IPatientRepository extends CrudRepository<Patient, Long> {
-}
